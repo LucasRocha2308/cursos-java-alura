789e6f8b4c0db36a920367ad969c8e22306a69aa
LucasRocha2308 <lucas_rocha_25@hotmail.com>
2022-02-28 16:49:30 -0300

    exercicio de Unchecked Exception

--- bytebank-herdado-conta/src/TesteSaca.java
+++ bytebank-herdado-conta/src/TesteSaca.java
@@ -5,7 +5,11 @@ public class TesteSaca {
 		Conta conta = new ContaCorrente(445, 446);
 		
 		conta.deposita(200.0);
-		conta.saca(210.0);
+		try {			
+			conta.saca(210.0);
+		}catch(SaldoInsuficienteException ex) {
+			System.out.println("Ex: " + ex.getMessage());
+		}
 		
 		System.out.println(conta.getSaldo());
 	}
